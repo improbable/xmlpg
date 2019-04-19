4bd1ace70c1f4ee91d3bcf74ddb18d0fff78b7b0
christophergatto <christophergatto@improbable.io>
2019-04-19 09:50:18 -0400

    Making this a spatial project for ease of test schema

---
+++ test_schema.sh
@@ -0,0 +1,6 @@
+echo "Generating csharp"
+spatial schema generate --cachePath=.spatialos/schema_codegen_cache --output=../../build/GeneratedCode/csharp --language=csharp
+echo "Generating java"
+spatial schema generate --cachePath=.spatialos/schema_codegen_cache --output=../../build/GeneratedCode/java --language=java
+echo "Generating cpp"
+spatial schema generate --cachePath=.spatialos/schema_codegen_cache --output=../../build/GeneratedCode/cpp --language=cpp
